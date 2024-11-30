53a9ac53a08a086553baf2e69831bcf6cfdbac07
Aleksandar Trifunovski <nesumtoj@gmail.com>
2024-11-30 20:07:58 +0100

    - swagger ui

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,8 +21,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // Enable the Swagger UI
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // This makes Swagger available at the root of the app (e.g., http://localhost:8080)
+    });
 }
 
 app.UseAuthorization();
